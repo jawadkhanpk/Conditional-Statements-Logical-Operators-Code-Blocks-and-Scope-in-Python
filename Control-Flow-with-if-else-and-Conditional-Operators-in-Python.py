333b94be265830bad460e50ccc2fc3e489220e35
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-27 20:06:55 +0500

    Basic if-else syntax

--- Control-Flow-with-if-else-and-Conditional-Operators-in-Python.py
+++ Control-Flow-with-if-else-and-Conditional-Operators-in-Python.py
@@ -1,1 +1,9 @@
-print("Hello World")
+# Basic Python Program for implementation of if-else condition
+
+print("Welcome to a Rollercoaster!")
+height = int(input("What is Your Height in Centimeters? "))
+
+if height > 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, You have to grow taller before you can ride rollercoaster!")
