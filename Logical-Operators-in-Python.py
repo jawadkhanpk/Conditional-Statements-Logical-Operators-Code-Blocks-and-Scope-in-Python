40c7b7b23ecd2bb92c4dd11f55f43d6e0f3362d1
JAWAD KHAN <jawadkhan.tech@gmail.com>
2024-04-28 13:37:01 +0500

    Logical-Operators-in-Python

---
+++ Logical-Operators-in-Python.py
@@ -0,0 +1,18 @@
+# Logical AND operator
+print("Logical AND operator:")
+print(True and True)    # Output: True
+print(True and False)   # Output: False
+print(False and True)   # Output: False
+print(False and False)  # Output: False
+
+# Logical OR operator
+print("\nLogical OR operator:")
+print(True or True)     # Output: True
+print(True or False)    # Output: True
+print(False or True)    # Output: True
+print(False or False)   # Output: False
+
+# Logical NOT operator
+print("\nLogical NOT operator:")
+print(not True)         # Output: False
+print(not False)        # Output: True
